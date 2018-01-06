892877b652777d4a33d62a2cb19c36fb90b9f50b
LiJiaHaO <598188200@qq.com>
2018-01-06 23:38:47 +0800

    初始化project 李佳豪 2018年1月6日 23:37:31

---
+++ src/main/java/blog/blogApplication.java
@@ -0,0 +1,14 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by DH on 2018/1/6.
+ */
+@SpringBootApplication
+public class blogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(blogApplication.class,args);
+    }
+}
